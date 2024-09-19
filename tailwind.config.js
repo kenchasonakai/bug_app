a757ad7fbf2faadd9ff56edf9c337352a0f0fee3
kenchasonakai <kenchasonakai@gmail.com>
2024-09-20 01:17:22 +0900

    add: daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const { default: daisyui } = require("daisyui");
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+    darkTheme: false,
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
 }
