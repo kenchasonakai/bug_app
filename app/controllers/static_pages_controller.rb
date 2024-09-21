e318840332e41b46edc6f6f29136cadc2b59453c
kenchasonakai <kenchasonakai@gmail.com>
2024-09-21 23:18:54 +0900

    fix: rubocop

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:top]
+  skip_before_action :authenticate_user!, only: [ :top ]
   def top; end
 end
