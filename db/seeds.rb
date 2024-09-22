f3dc7712e2d99965c9691a196a8f601965ec89e4
kenchasonakai <kenchasonakai@gmail.com>
2024-09-22 21:42:48 +0900

    add: seedの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Start inserting seed data"
+user1 = User.find_or_initialize_by(nickname: "Alice", email: "alice@example.com")
+user1.password = "password"
+puts user1.save ? "User1 created" : "User1 creation failed"
+
+user2 = User.find_or_initialize_by(nickname: "Bob", email: "bob@example.com")
+user2.password = "password"
+puts user2.save ? "User2 created" : "User2 creation failed"
+
+tag_array = %w[Ruby Rails JavaScript TypeScript React ハンバーグ ラーメン カレー ピザ パスタ]
+
+10.times do
+  user1.posts.create!(title: Faker::Book.title, content: Faker::Markdown.sandwich(sentences: 10, repeat: 10), tag_list: tag_array.sample(3).join(" "))
+end
+
+10.times do
+  user2.posts.create!(title: Faker::Book.title, content: Faker::Markdown.sandwich(sentences: 10, repeat: 10), tag_list: tag_array.sample(3).join(" "))
+end
