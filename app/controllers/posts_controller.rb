2b8840bb8ac240c311060605167be943f31650e8
kenchasonakai <kenchasonakai@gmail.com>
2024-09-25 19:49:30 +0900

    fix: index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,6 @@ class PostsController < ApplicationController
   def index
     if params[:tag].present? && params[:tag].is_a?(String)
       @posts = Post.tagged_with(params[:tag].strip).order(created_at: :desc)
-    else
-      @posts = Post.all.order(created_at: :desc)
     end
   end
 
