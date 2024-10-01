1ec9f3603793a0d38d2599d60f2c9cc728b922d1
kenchasonakai <kenchasonakai@gmail.com>
2024-10-01 19:52:31 +0900

    fix: エラー内容がわかりにくいので削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: %i[index show]
   def index
     if params[:tag].present? && params[:tag].is_a?(String)
       @posts = Post.tagged_with(params[:tag].strip).order(created_at: :desc)
